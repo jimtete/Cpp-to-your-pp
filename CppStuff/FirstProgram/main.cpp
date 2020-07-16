82f956d52f779b929efee75f3e19d120ab520094
Dimitris Tetepoulidis <48908714+jimtete@users.noreply.github.com>
2020-07-16 20:17:02 +0300

    update 15072020

---
+++ CppStuff/FirstProgram/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(){
+	int favouriteNumber;
+	std::cout<<"Give me your favourite number between 1 and 100: ";
+	
+	std::cin >> favouriteNumber;
+	
+	std::cout << "That is my favourite number too"<< std::endl;
+	
+	return 0;
+}
